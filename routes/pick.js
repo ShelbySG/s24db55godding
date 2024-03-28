a16ee684abe7ef21f1cb7729d2254cde6a332e60
Shelby Godding <s539355@nwmissouri.edu>
2024-03-27 23:31:06 -0500

    choose

--- routes/pick.js
+++ routes/pick.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  let query = req.query;
-  console.log(`rows ${query.rows}`);
-  console.log(`cols ${query.cols}`);
-  res.render('grid', { title: "Grid" , query: query});
+  res.render('randomitem', { title: 'A random item' });
 });
 
 module.exports = router;
