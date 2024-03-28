5c9c6a5834c2beb37fff89ee5d0f39e8cd441e13
Shelby Godding <s539355@nwmissouri.edu>
2024-03-27 20:21:49 -0500

    endpoint created

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var EasterBasketRouter = require('./routes/easterbasket');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/easterBasket', EasterBasketRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
